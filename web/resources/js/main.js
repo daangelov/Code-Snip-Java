68cba6050a31be4224b5f7f280e4b015ff20e72d
daangelov <a.dragomir1996@gmail.com>
2020-02-08 16:21:05 +0200

    Add vendor libraries and fix styles

---
+++ web/resources/js/main.js
@@ -0,0 +1,11 @@
+function showSpinner() {
+    document.getElementById('#loading_div');
+    document.getElementById('#loading_gif');
+    // TODO show
+}
+
+function hideSpinner() {
+    document.getElementById('#loading_div');
+    document.getElementById('#loading_gif');
+    // TODO hide
+}
